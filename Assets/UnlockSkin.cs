46abafaa9e8d47317701fc605df68d02538686e9
Eray Kurtulus <erayk96@gmail.com>
2020-07-12 13:39:14 +0300

    Finished development. When there remains only a few locked skins, the random green selection can remain on the same skin, worsening the user experience. Maybe it can be updated so that the next "chosen" skin cannot be the same as the previous chosen.

--- Assets/UnlockSkin.cs
+++ Assets/UnlockSkin.cs
@@ -9,8 +9,8 @@ public class UnlockSkin : MonoBehaviour {
     Color green;
     Color gray;
     System.Random rand;
-    GameObject[] skins;
-    List<GameObject> lockedSkins;
+    GameObject[] skins; // this will hold all skins
+    List<GameObject> lockedSkins; // this will hold locked skins
     public Button unlockSkinButton;
     public Sprite[] skinSprites = new Sprite[9];
 
@@ -23,35 +23,64 @@ public class UnlockSkin : MonoBehaviour {
 
         rand = new System.Random();
 
+        // find all skins (skin buttons)
         skins = GameObject.FindGameObjectsWithTag("SkinButton");
+        // initialize the lockedSkins list
         lockedSkins = new List<GameObject>();
 
+        // for each skin, if its SkinImage child is tagged "LockedSkin",
+        // add it to the lockedSkins list
         foreach (GameObject skin in skins) {
             if(skin.transform.GetChild(1).tag == "LockedSkin") {
                 lockedSkins.Add(skin);
             }
         }
 
+        // add on-click-listener to the unlock skin button
         unlockSkinButton.onClick.AddListener(onClick);
     }
 
     void onClick() {
-        StartCoroutine(unlockSkin());
+        // if there are any locked skins left
+        if(lockedSkins.Count > 0) {
+            // start a coroutine so that waiting does not freeze the app
+            StartCoroutine(unlockSkin());
+            // TODO? think of a solution so that the button is not spammed
+            // if the game requires a currency to unlock skins, this won't be an issue
+        } else {
+            // TODO? prompt a notification that says all skins are unlocked
+            // or remove the unlock skin button?
+            Debug.Log("All skins are unlocked!");
+        }
     }
 
     IEnumerator unlockSkin() {
-        int chosen;
-        for(int i = 0; i < 10; i++) {
-            chosen = rand.Next(lockedSkins.Count);
-            lockedSkins[chosen].GetComponent<Image>().color = green;
-            // wait a little
-            yield return new WaitForSeconds(0.1f * i);
-            lockedSkins[chosen].GetComponent<Image>().color = gray;
+
+        int chosen; // randomly chosen locked skin
+
+        // if there are more than 1 lockedSkins
+        if(lockedSkins.Count > 1) {
+            // fake-choose 8 times, paint green and then gray
+            for(int i = 0; i < 8; i++) {
+                chosen = rand.Next(lockedSkins.Count);
+                lockedSkins[chosen].GetComponent<Image>().color = green;
+                // wait a little (increasingly more)
+                yield return new WaitForSeconds(0.1f * i);
+                lockedSkins[chosen].GetComponent<Image>().color = gray;
+            }
         }
+
+        // choose for real, paint green
         chosen = rand.Next(lockedSkins.Count);
         lockedSkins[chosen].GetComponent<Image>().color = green;
-        Debug.Log(lockedSkins[chosen].name[4] - '1');
-        lockedSkins[chosen].transform.GetChild(1).GetComponent<Image>().sprite = skinSprites[(lockedSkins[chosen].name[4] - '1')];
+
+        // get the SkinImage child, show real sprite, tag it as "Untagged"
+        Transform skinImage = lockedSkins[chosen].transform.GetChild(1);
+        skinImage.GetComponent<Image>().sprite = skinSprites[(lockedSkins[chosen].name[4] - '1')];
+        skinImage.tag = "Untagged";
+
+        // remove chosen skin from the lockedSkins list
+        lockedSkins.RemoveAt(chosen);
     }
 
 }
